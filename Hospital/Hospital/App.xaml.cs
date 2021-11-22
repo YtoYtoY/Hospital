0a6394277972c1273b8e9f289c6f24bc25b7229b
YtoYtoY <69724250+YtoYtoY@users.noreply.github.com>
2021-11-23 00:02:27 +0300

    Добавьте файлы проекта.

---
+++ Hospital/Hospital/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Hospital.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Hospital
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new LanguagePage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
